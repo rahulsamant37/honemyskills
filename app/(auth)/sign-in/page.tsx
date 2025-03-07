a1ba7b938759496ee9cf03a6ccead445fb28a3aa
rahulsamant37 <rahulsamantcoc2@gmail.com>
2025-03-08 00:24:33 +0530

    Convex Ingestion for Sign-In

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,38 +1,48 @@
 "use client"
 import { Button } from '@/components/ui/button'
+import { api } from '@/convex/_generated/api';
 import { GetAuthUserData } from '@/services/GlobalApi';
 import { useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
+import { useMutation } from 'convex/react';
 import Image from 'next/image'
 import React from 'react'
 
 function SignIn() {
   
-const googleLogin = useGoogleLogin({
-  onSuccess: async (tokenResponse) => {
-    console.log(tokenResponse);
+  const CreateUser=useMutation(api.users.CreateUser);
+  const googleLogin = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      console.log(tokenResponse);
 
-    if (typeof window !== undefined) {
-      localStorage.setItem('user_token',tokenResponse.access_token)
-    }
-    const user=GetAuthUserData(tokenResponse.access_token);
-    console.log(user);
-  },
-  onError: errorResponse => console.log(errorResponse),
-});
-  return (
-    <div className='flex items-center flex-col justify-center h-screen'>
-        <div className='flex flex-col items-center
-        gap-3 border rounded-2xl p-10 shadow-md'>
-            <Image src={'/logo.svg'} alt='logo'
-                width={100}
-                height={100}
-            />
-            <h2 className='text-2xl'>Sign In To honemyskills Website</h2>
-            <Button onClick={()=>googleLogin()}>Sign In With Gmail</Button>
-        </div>
-    </div>
-  )
+      if (typeof window !== undefined) {
+        localStorage.setItem('user_token',tokenResponse.access_token)
+      }
+      const user = await GetAuthUserData(tokenResponse.access_token);
+      console.log(user);
+      // Save User Info
+      const result = await CreateUser({
+        name:user?.name,
+        email:user?.email,
+        picture:user?.picture
+      });
+      console.log("--", result);
+    },
+    onError: errorResponse => console.log(errorResponse),
+  });
+    return (
+      <div className='flex items-center flex-col justify-center h-screen'>
+          <div className='flex flex-col items-center
+          gap-3 border rounded-2xl p-10 shadow-md'>
+              <Image src={'/logo.svg'} alt='logo'
+                  width={100}
+                  height={100}
+              />
+              <h2 className='text-2xl'>Sign In To honemyskills Website</h2>
+              <Button onClick={()=>googleLogin()}>Sign In With Gmail</Button>
+          </div>
+      </div>
+    )
 }
 
 export default SignIn
