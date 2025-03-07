cd565ab4a9a7187ec3186f2b04073811e9664600
rahulsamant37 <rahulsamantcoc2@gmail.com>
2025-03-08 01:38:15 +0530

    adding ai-assistants header

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -7,12 +7,14 @@ import { useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
 import { useMutation } from 'convex/react';
 import Image from 'next/image'
+import { useRouter } from 'next/navigation';
 import React, { useContext } from 'react'
 
 function SignIn() {
   
   const CreateUser=useMutation(api.users.CreateUser);
   const {user, setUser} = useContext(AuthContext);
+  const router = useRouter();
   const googleLogin = useGoogleLogin({
     onSuccess: async (tokenResponse) => {
       console.log(tokenResponse);
@@ -29,6 +31,7 @@ function SignIn() {
       });
       console.log("--", result);
       setUser(result);
+      router.replace('/ai-assistant')
     },
     onError: errorResponse => console.log(errorResponse),
   });
---
+++ app/(main)/_components/Header.tsx
@@ -0,0 +1,24 @@
+"use client"
+import { AuthContext } from '@/context/AuthContext'
+import Image from 'next/image'
+import React, { useContext } from 'react'
+
+function Header() {
+    const {user} = useContext(AuthContext);
+    return (
+      <div className='p-2 shadow-sm flex justify-between items-center px-7'>
+          <Image src={'/logo.svg'} alt='logo'
+            width={30}
+            height={30}
+          />
+
+          {user?.picture && <Image src={user?.picture} alt='logo'
+            width={30}
+            height={30}
+            className='rounded-full'
+          />}
+      </div>
+    )
+}
+
+export default Header
