80d16ef7d57171290817a5e93a19afa92f555871
rahulsamant37 <rahulsamantcoc2@gmail.com>
2025-03-07 19:46:54 +0530

    basic project structure

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Hello Welcome to My World!</h2>
+      <Button>Don't Click</Button>
+    </div>
+  );
+}
---
+++ app/provider.tsx
@@ -0,0 +1,25 @@
+"use client"
+import React from 'react'
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+function Provider({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+  return (
+    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
+            <NextThemesProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+        >
+            <div>{children}</div>
+        </NextThemesProvider>
+    </GoogleOAuthProvider>
+  )
+}
+
+export default Provider
