3620afb5f8744672a9cb531783afd780696723d0
rahulsamant37 <rahulsamantcoc2@gmail.com>
2025-03-08 00:09:59 +0530

    Initializing Convex for Database

--- app/provider.tsx
+++ app/provider.tsx
@@ -2,22 +2,26 @@
 import React from 'react'
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { ConvexProvider, ConvexReactClient } from 'convex/react';
 
 function Provider({
     children,
   }: Readonly<{
     children: React.ReactNode;
   }>) {
+    const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
   return (
     <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
+        <ConvexProvider client={convex}>
             <NextThemesProvider
-        attribute="class"
-        defaultTheme="dark"
-        enableSystem
-        disableTransitionOnChange
-        >
-            <div>{children}</div>
-        </NextThemesProvider>
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+          >
+              <div>{children}</div>
+          </NextThemesProvider>
+        </ConvexProvider>
     </GoogleOAuthProvider>
   )
 }
