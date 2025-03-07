6baa617e2c86e9c83f6d6870784196cfd7f1fda6
rahulsamant37 <rahulsamantcoc2@gmail.com>
2025-03-08 03:44:08 +0530

    Setting up the Route for Workspace and defining schema for ai assistants for user

--- app/(main)/ai-assistants/page.tsx
+++ app/(main)/ai-assistants/page.tsx
@@ -1,9 +1,15 @@
 "use client"
+import { BlurFade } from '@/components/magicui/blur-fade'
 import { Button } from '@/components/ui/button'
 import { Checkbox } from '@/components/ui/checkbox'
+import { AuthContext } from '@/context/AuthContext'
+import { api } from '@/convex/_generated/api'
 import AiAssistantsList from '@/services/AiAssistantsList'
+import { useConvex, useMutation } from 'convex/react'
+import { Loader2Icon } from 'lucide-react'
 import Image from 'next/image'
-import React, { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import React, { useContext, useEffect, useState } from 'react'
 
 export type ASSISTANT = {
     id: number,
@@ -16,7 +22,29 @@ export type ASSISTANT = {
 }
 
 function AIAssistants() {
-    const [selectedAssistant,setSelectedAssistant]=useState<ASSISTANT[]>([]);
+    const [selectedAssistant,setSelectedAssistant] = useState<ASSISTANT[]>([]);
+    const insertAssistant = useMutation(api.userAiAssistants.InsertSelectedAssistants);
+    const {user} = useContext(AuthContext);
+    const [loading,setLoading] = useState(false);
+    const convex = useConvex();
+    const router = useRouter();
+
+    useEffect(()=>{
+        user&&GetUserAssistants();
+    },[user])
+
+    const GetUserAssistants = async() => {
+        const result = await convex.query(api.userAiAssistants.GetAllUseAssistants,{
+            uid:user._id
+        });
+        console.log(result)
+        if (result.length>0)
+        {
+            //Navigate to New Screen
+            router.replace('/workspace')
+            return ;
+        }
+    }
 
     const onSelect = (assistant: ASSISTANT)=>
     {
@@ -32,29 +60,47 @@ function AIAssistants() {
         return item?true:false
     }
 
+    const OnClickContinue = async() => {
+        setLoading(true);
+        	const result =  await insertAssistant({
+                records:selectedAssistant,
+                uid:user?._id
+            });
+            setLoading(false);
+            console.log(result);
+    }
+
     return (
       <div className='px-10 mt-15 md:px-16 ls:px-36 xl:px-48'>
           <div className='flex justify-between items-center'>
-              <div>
-                  <h2 className='text-3xl font-bold'>Welcome to World of AI Assistants🤖</h2>
-                  <p className='text-xl mt-2'>Choose your AI Campanion to Simplify Your Task 🚀</p>
-              </div>
-              <Button className='cursor-pointer'>Continue</Button>
+                <div>
+                    <BlurFade delay={0.25} inView>
+                        <h2 className='text-3xl font-bold'>Welcome to World of AI Assistants🤖</h2>
+                    </BlurFade>
+                    <BlurFade delay={0.25 * 2} inView>
+                        <p className='text-xl mt-2'>Choose your AI Campanion to Simplify Your Task 🚀</p>
+                    </BlurFade>
+                </div>
+              <Button disabled={selectedAssistant?.length == 0||loading} className='cursor-pointer'
+              onClick={OnClickContinue}
+              > {loading&&<Loader2Icon className='animate-spin'/>} Continue</Button>
           </div>
 
           <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 mt-5'>
               {AiAssistantsList.map((assistant, index)=>(
-                  <div key={index} className='hover:border p-3 rounded-xl hover:scale-105 
-                  transition-all ease-in-out cursor-pointer relative' onClick={()=>onSelect(assistant)}>
-                      <Checkbox className='absolute m-2' checked={IsAssistantSelected(assistant)} />
-                      <Image src={assistant.image} alt={assistant.title}
-                          width={600}
-                          height={600}
-                          className='rounded-xl w-full h-[200px] object-cover'
-                      />
-                      <h2 className='text-center font-bold text-lg'>{assistant.name}</h2>
-                      <h2 className='text-center text-gray-600 dark:text-gray-300'>{assistant.title}</h2>
-                  </div>
+                <BlurFade key={assistant.image} delay={0.25 + index * 0.05} inView>
+                    <div key={index} className='hover:border p-3 rounded-xl hover:scale-105 
+                    transition-all ease-in-out cursor-pointer relative' onClick={()=>onSelect(assistant)}>
+                        <Checkbox className='absolute m-2' checked={IsAssistantSelected(assistant)} />
+                        <Image src={assistant.image} alt={assistant.title}
+                            width={600}
+                            height={600}
+                            className='rounded-xl w-full h-[200px] object-cover'
+                        />
+                        <h2 className='text-center font-bold text-lg'>{assistant.name}</h2>
+                        <h2 className='text-center text-gray-600 dark:text-gray-300'>{assistant.title}</h2>
+                    </div>
+                </BlurFade>
               ))}
           </div>
       </div>
---
+++ convex/userAiAssistants.ts
@@ -0,0 +1,34 @@
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+import { handler } from "tailwindcss-animate";
+
+export const InsertSelectedAssistants=mutation({
+    args:{
+        records:v.any(),
+        uid:v.id('users')
+    },
+    handler:async(ctx,args)=>{
+        const insertedIds = await Promise.all(
+            args.records.map(async(record:any)=>
+            await ctx.db.insert('userAiAssistants',{
+                ...record,
+                uid:args.uid
+            })
+            )
+        );
+        return insertedIds;
+    }
+})
+
+export const GetAllUseAssistants=query({
+    args:{
+        uid:v.id('users')
+    },
+    handler:async(ctx,args)=>{
+        const result = await ctx.db.query('userAiAssistants')
+            .filter(q=>q.eq(q.field('uid'),args.uid))
+            .collect();
+
+        return result;
+    }
+})
